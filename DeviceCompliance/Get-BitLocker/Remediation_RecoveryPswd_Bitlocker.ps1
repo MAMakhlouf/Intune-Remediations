7dbadc317fbd2e917b71203838a5e9f4a2796d94
MAMakhlouf <107413891+MAMakhlouf@users.noreply.github.com>
2024-11-26 11:16:36 +0100

    Add files via upload

---
+++ DeviceCompliance/Get-BitLocker/Remediation_RecoveryPswd_Bitlocker.ps1
@@ -0,0 +1,11 @@
+# Generate a recovery password
+ $RecoveryPasswordProtector = Add-BitLockerKeyProtector -MountPoint "C:" -RecoveryPasswordProtector
+
+# Retrieve the generated recovery password
+$RecoveryPassword = $RecoveryPasswordProtector.KeyProtector.RecoveryPassword[0]
+ Write-Output "BitLocker recovery password: $RecoveryPassword"
+
+# Enable BitLocker with recovery password
+Enable-BitLocker -MountPoint "C:" -RecoveryPasswordProtector -RecoveryPassword $RecoveryPassword -SkipHardwareTest -EncryptionMethod XtsAes256
+
+Write-Output "BitLocker has been enabled on the system drive."
